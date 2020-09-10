042474cb5215e70501a4d79791018d679e2219bc
Rinatxxx07 <rinatxxx07@gmail.com>
2020-09-10 07:28:08 +0500

    added author section

--- App/js/main.js
+++ App/js/main.js
@@ -1,1 +1,16 @@
-$(function () {});
+$(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $(".product-slider-inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
+  var Mixer = mixitup(".products__inner-box");
+});
